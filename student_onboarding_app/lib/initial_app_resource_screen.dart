4f57df6c7aec7f58ea8eba0ff79a9a65df9cb350
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-04-06 19:29:39 +0530

    created the initial app screen.

---
+++ student_onboarding_app/lib/initial_app_resource_screen.dart
@@ -0,0 +1,10 @@
+import 'package:student_onboarding_app/utils/common_exports.dart';
+
+class InitialAppResourceScreen extends StatelessWidget {
+  const InitialAppResourceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ student_onboarding_app/lib/utils/common_exports.dart
@@ -0,0 +1,2 @@
+export 'package:flutter/material.dart';
+export 'package:student_onboarding_app/initial_app_resource_screen.dart';
