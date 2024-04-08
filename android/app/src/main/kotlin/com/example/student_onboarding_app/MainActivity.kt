54c889399a0683a494c70fa51f41532cea0c7e04
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-04-08 22:57:01 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/student_onboarding_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.student_onboarding_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StudentOnboardApp());
+}
+
+class StudentOnboardApp extends StatelessWidget {
+  const StudentOnboardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
