9c4af17dd2cac6ef4989664272b57a58a1b58725
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-04-09 15:11:09 +0530

    created the initial resources app file.

---
+++ lib/initial_resources_app.dart
@@ -0,0 +1,21 @@
+
+import 'package:student_onboarding_app/utils/common_exports.dart';
+
+class InitialResourceApp extends StatefulWidget {
+  const InitialResourceApp({super.key});
+
+  @override
+  State<InitialResourceApp> createState() => _InitialResourceAppState();
+}
+
+class _InitialResourceAppState extends State<InitialResourceApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.pushReplacement(Home());
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
