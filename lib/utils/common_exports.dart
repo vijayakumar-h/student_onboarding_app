a5d6dc5bbeac192a922ddfffff546dc00df7c7e3
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-04-09 15:09:29 +0530

    created the common exports file.

---
+++ lib/utils/common_exports.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+export 'package:student_onboarding_app/home.dart';
+export 'package:student_onboarding_app/initial_resources_app.dart';
+export 'package:student_onboarding_app/utils/build_context_extension.dart';
