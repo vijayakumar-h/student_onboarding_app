0ca08558a2f84c61e4deb7be862d98bb0a8f6ca5
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-04-09 15:10:46 +0530

    created the home screen.

---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:student_onboarding_app/utils/common_exports.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text("sdv"),
+          Text("sdv"),
+          Text("sdv"),
+          Text("sdv"),
+          Text("sdv"),
+        ],
+      ),
+    );
+  }
+}
