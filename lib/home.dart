2eaa45bb4aa29e763e91166d1d1fac822836215a
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-04-28 19:33:28 +0530

    created the app theme setup file.

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:student_onboarding_app/utils/common_exports.dart';
 
 class Home extends StatelessWidget {
@@ -7,17 +8,50 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
       ),
-      body: Column(
-        children: [
-          Text("sdv"),
-          Text("sdv"),
-          Text("sdv"),
-          Text("sdv"),
-          Text("sdv"),
-        ],
+      bottomNavigationBar: const BottomNavigationBarItemWidget(),
+      body: const Column(
+        children: [],
       ),
+    );
+  }
+}
+
+class BottomNavigationBarItemWidget extends StatelessWidget {
+  const BottomNavigationBarItemWidget({super.key});
+
+  static Widget _returnBottomNavIcon(String icon,
+          {Color? color, IconData? iconData, double? width}) =>
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4),
+        child: iconData != null
+            ? Icon(iconData, size: 20, color: color)
+            : SvgPicture.string(
+                icon,
+                height: width ?? 20,
+                color: color,
+              ),
+      );
+
+  // static Widget _returnBottomNavIconIos(String icon, BuildContext context,{Color? color, IconData? iconData, double? widtht }) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          label: "Home",
+          icon: Icon(Icons.home),
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Person"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined), label: "Calendar"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.currency_exchange_outlined), label: "Currency"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_added_sharp), label: "bookMark"),
+      ],
     );
   }
 }
--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -1,5 +1,14 @@
 import 'package:student_onboarding_app/utils/common_exports.dart';
 
 class AppTheme {
-  ThemeData get appTheme => ThemeData();
+  ThemeData get appTheme => ThemeData(
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.black,
+          selectedLabelStyle: TextStyle(color: Colors.deepPurple),
+          unselectedLabelStyle: TextStyle(color: Colors.black),
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+        ),
+      );
 }
