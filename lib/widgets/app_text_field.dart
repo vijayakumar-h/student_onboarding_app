8da7cdb488abb9f5d588b869d44c11d59f0e1b7a
VijayHonnegowda <vijay.kumar@repalogic.com>
2024-05-15 21:54:18 +0530

    created the app text field widget.

---
+++ lib/widgets/app_text_field.dart
@@ -0,0 +1,44 @@
+import 'package:student_onboarding_app/utils/common_exports.dart';
+
+class AppTextField extends StatelessWidget {
+  final String? titleText;
+  final String? placeHolder;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+
+  const AppTextField(
+      {super.key,
+      this.titleText,
+      this.placeHolder,
+      this.prefixIcon,
+      this.suffixIcon});
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          if (titleText != null) ...{Text(titleText!)},
+          TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey.withOpacity(0.3),
+                ),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.red.withOpacity(0.3),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey.withOpacity(0.3),
+                ),
+              ),
+              hintText: placeHolder,
+              prefixIcon: prefixIcon,
+              suffixIcon: suffixIcon,
+            ),
+          ),
+        ],
+      );
+}
