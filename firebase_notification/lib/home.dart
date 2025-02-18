6776ba729c613bda3a7712c9630f0a3d8f3ed818
VijayHonnegowda <vijay.kumar@repalogic.com>
2025-02-18 10:44:40 +0530

    testing

--- firebase_notification/lib/home.dart
+++ firebase_notification/lib/home.dart
@@ -5,6 +5,15 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notification"),
+      ),
+      body: const Column(
+        children: [
+          Text("data"),
+        ],
+      ),
+    );
   }
 }
